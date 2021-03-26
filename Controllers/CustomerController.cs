c40967e94a0fcd6e7f165595ee867ccbe7b6a0e4
ea682 <erick.ahumada@lag.cl>
2021-03-25 22:55:30 -0300

    Se crea ejemplo del ruteo de una API

---
+++ Controllers/CustomerController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NorthwindApiDemo.Controllers
+{
+    [Route("api/customers")]
+    public class CustomerController : Controller
+    {
+        [HttpGet]
+        public JsonResult GetCustomers()
+        {
+            return new JsonResult(new List<Object>()
+            {
+                new { CustomerId = 1, ContactName = "Anderson"},
+                new { CustomerId = 2, ContactName = "Solaris"},
+            }); ;
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -16,7 +16,7 @@ namespace NorthwindApiDemo
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
+            services.AddControllers(options => options.EnableEndpointRouting = false);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -27,16 +27,23 @@ namespace NorthwindApiDemo
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseRouting();
-
             app.UseStatusCodePages();
+
+            app.UseMvc();
+            /*app.UseMvc(config => 
+            {
+                config.MapRoute(
+                    name: "Default",
+                    template: "{controller}/{action}/{id?}",
+                    defaults: new 
+                    {
+                        controller = "Home",
+                        action = "Index"
+                    });
+            });*/
             /*app.UseEndpoints(endpoints =>
             {
                 endpoints.MapGet("/", async context =>
-                {
-                    throw new Exception("Testin Exceptions");
-                });
-                /*endpoints.MapGet("/", async context =>
                 {
                     await context.Response.WriteAsync("Hello World!");
                 });
